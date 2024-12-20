2c30102ea17ff35b4a816b0b1a40712a3a37d294
David England <david.england.de@googlemail.com>
2024-12-20 08:57:44 +0000

    Create and inject stub Solver service

---
+++ SolverZeroUI/Services/ISolver.cs
@@ -0,0 +1,7 @@
+﻿namespace SolverZeroUI.Services
+{
+    public interface ISolver
+    {
+        Task<List<int[,]>> Solve(int[,] input);
+    }
+}
---
+++ SolverZeroUI/Services/Solver.cs
@@ -0,0 +1,16 @@
+﻿namespace SolverZeroUI.Services
+{
+    public class Solver : ISolver
+    {
+        public async Task<List<int[,]>> Solve(int[,] input)
+        {
+            // Ignore input for now.
+
+            string json = File.ReadAllText("./Samples/basic.json");
+
+            Console.WriteLine(json);
+
+            return new List<int[,]> { new int[0,0] };
+        }
+    }
+}
