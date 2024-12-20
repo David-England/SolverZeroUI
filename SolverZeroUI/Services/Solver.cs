d9e875e7b652f91edaf1ef2b8e29e5357bc94c80
David England <david.england.de@googlemail.com>
2024-12-20 09:28:39 +0000

    Invoke web service, albeit by hardcoding

--- SolverZeroUI/Services/Solver.cs
+++ SolverZeroUI/Services/Solver.cs
@@ -2,13 +2,25 @@
 {
     public class Solver : ISolver
     {
+        private HttpClient _httpClient;
+
+        public Solver()
+        {
+            _httpClient = new HttpClient();
+        }
+
         public async Task<List<int[,]>> Solve(int[,] input)
         {
             // Ignore input for now.
 
             string json = File.ReadAllText("./Samples/basic.json");
 
-            Console.WriteLine(json);
+            HttpRequestMessage message = new(HttpMethod.Get, "http://localhost:8081/sudoku")
+                { Content = new StringContent(json) };
+
+            HttpResponseMessage response = await _httpClient.SendAsync(message);
+
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
 
             return new List<int[,]> { new int[0,0] };
         }
